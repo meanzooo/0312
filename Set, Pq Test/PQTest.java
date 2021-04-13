ed562e758cc2ee42bea214381998f5fa7ff54042
meanzooo <80101715+meanzooo@users.noreply.github.com>
2021-04-13 16:43:01 +0900

    Add files via upload

---
+++ Set, Pq Test/PQTest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PQTest {
+	public static void main(String[] args) {
+		test1();
+	}
+	
+	public static void test1() {
+		Queue<String> q = new PriorityQueue<>();
+		q.offer("PineApple"); // = q.add()
+		q.offer("Apple");
+		q.offer("Grape");
+		q.offer("Lime");
+		q.offer("Coconut");
+		
+		//¿³º¸±â. ÀÎÃâÀº ¾Æ´Ô.
+		System.out.print(q.peek()+" "); // = q.element()
+		System.out.print(q.peek()+" ");
+		System.out.print(q.peek()); // ´Ù °°Àº °Å ³ª¿È
+		System.out.println();
+		
+		while(q.size()>0) {
+			System.out.println(q.poll()); // = q.remove()
+		}
+	}
+}
